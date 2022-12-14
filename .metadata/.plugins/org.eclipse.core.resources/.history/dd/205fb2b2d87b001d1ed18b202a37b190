8fc08354dfd3475d973ba009776ad83287379b44
SilvaAvelinoAndre <andreavelinosilva@hotmail.com>
2022-12-14 15:45:22 -0300

    Criando Tb no bd e generiDao com Salvar

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/dd/205fb2b2d87b001d1ed18b202a37b190
@@ -0,0 +1,14 @@
+package pos.java.maven.hibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+	
+	@Test
+	public void TesteHibernateUtil() {
+		
+		HibernateUtil.getEntityManager();
+		
+	}
+
+}
--- pos-java-maven-hibernate/src/test/java/pos/java/maven/hibernate/TesteHibernate.java
+++ pos-java-maven-hibernate/src/test/java/pos/java/maven/hibernate/TesteHibernate.java
@@ -1,14 +1,37 @@
 package pos.java.maven.hibernate;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import dao.GenericDao;
+import model.UsuarioPessoa;
 
 public class TesteHibernate {
 	
 	@Test
+	@Ignore
 	public void TesteHibernateUtil() {
 		
 		HibernateUtil.getEntityManager();
 		
 	}
 
+	@Test
+	public void TesteDaoSalvar() {
+		
+		GenericDao<UsuarioPessoa> genericDao = new GenericDao<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setNome("katarina");
+		pessoa.setSobrenome("da Cunha Pires");
+		pessoa.setIdade(45);
+		pessoa.setLogin("katarinapires");
+		pessoa.setSenha("5678910");
+		pessoa.setEmail("katarinapires@gmail.com");
+		
+		genericDao.salvar(pessoa);
+		
+	}
+	
 }
